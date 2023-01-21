6a86ac8df6bd169e17cd89f6b4a5a53ef07d4c14
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-01-21 14:12:23 +0500

    commit

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser,AbstractBaseUser
+from django.contrib.auth.models import AbstractUser,AbstractBaseUser,UserManager
 from django.core.validators import RegexValidator
+import random
 
 ORDINARY_USER, MANAGER, SUPER_ADMIN = (
     'ordinary_user',
@@ -19,6 +20,21 @@ MALE,FEMALE=(
     "femail"
 )
 
+PHONE_EXPIRE=2
+EMAIL_EXPIRE=5
+
+class UserConfirmation(models.Model):
+    TYPE_CHOICES=(
+        (VIA_PHONE, VIA_PHONE),
+        (VIA_EMAIL, VIA_EMAIL)
+    )
+
+    code=models.CharField(max_length=4)
+    user=models.ForeignKey('users.User',on_delete=models.CASCADE)
+    verify_type=models.CharField(max_length=31, choices=TYPE_CHOICES)
+    expiration_time=models.DataTimeField(null=True)
+
+
 
 
 class User(AbstractUser):
@@ -36,7 +52,7 @@ class User(AbstractUser):
     AUTH_TYPE_CHOICES=(
         (VIA_EMAIL,VIA_EMAIL),
         (VIA_PHONE,VIA_PHONE),
-        (VIA_USERNAME,VIA_USERNAME),
+        (VIA_USERNAME,VIA_USERNAME)
     )
     SEX_CHOICES=(
         (MALE,MALE),
@@ -44,3 +60,29 @@ class User(AbstractUser):
     )
 
     
+    user_roles=models.CharField(max_length=31,choices=USER_ROLES, default=ORDINARY_USER)
+    auht_type=models.CharField(max_length=35, choices=AUTH_TYPE_CHOICES, default=VIA_USERNAME)
+    sex=models.CharField(max_length=20, choices= SEX_CHOICES, null=True)
+    email=models.EmailField(null=True,unique=True)
+    phone_number=models.CharField(max_length=12,null=True, unique=True,validators=[_validate_phone])
+    bio=models.CharField(max_length=200, null=True)
+
+    
+    object=UserManager()
+
+
+    def __str__(self):
+        return self.username
+
+    
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+    def create_verify_code(self,verify_type):
+        code="".join([str(random.randint(0,100)%10) for _ in range(4)])
+
+
+    
+
